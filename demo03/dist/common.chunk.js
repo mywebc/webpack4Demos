25fbeb85e0efc47343acecd89ca577cac42bee8e
陈晓拉尼 <2524729719@qq.com>
2018-09-09 22:42:20 +0800

    基本完成

---
+++ demo03/dist/common.chunk.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["common"],{
+
+/***/ "./c.js":
+/*!**************!*\
+  !*** ./c.js ***!
+  \**************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (c);\n\n//# sourceURL=webpack:///./c.js?");
+
+/***/ })
+
+}]);
---
+++ demo03/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path")
+// 多页面打包，提取公共代码
+module.exports = {
+    entry: {
+        a: './a.js',
+        b: './b.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        chunkFilename: "[name].chunk.js", // 非入口打包文件
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        splitChunks: {
+          cacheGroups: {
+            commons: {
+                name: 'common', // 提取公共chunk的名字
+                priority: 0, // 缓存优先级
+                chunks: 'initial', // 作用范围
+                minChunks: 2,//最小引用2次
+                minSize: 0 // 只要超出0字节就生成一个新包
+              },
+              vendor: {
+                name: "vendor",
+                test: /node_modules/,
+                chunks: "initial",
+                priority: 10,
+                chunks: 'all'
+              }
+          }
+        }
+      }
+}
